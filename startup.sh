85a7298a6f42299ea120ebef8814a81d7fe14676
LeeDaeeun <55821132+2daeeun@users.noreply.github.com>
2022-03-01 07:48:04 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Thinkpad_trackpoint_speed(빨콩_속도조절)
+#Thinkpad_trackpoint_speed(부팅시_빨콩_속도조절)
 xinput --set-prop "TPPS/2 Elan TrackPoint" "libinput Accel Speed" -1.0 &
 #Disable_bluetooth_on_boot(부팅시_블루투스_비활성화)
 rfkill block bluetooth &
-#enable_hangul_to_fcitx(fcitx를_이용해서_한글_활성화)
+#enable_hangul_to_fcitx(부팅시_fcitx를_이용해서_한글_활성화)
 fcitx &
-#tlp 실행
+#부팅시_tlp_자동_실행
 tlp start &
